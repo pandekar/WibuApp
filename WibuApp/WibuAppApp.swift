fa36609d18f89e05b1ff16a8ca64b05b74a2856c
pandekar <pandeadhistanaya@gmail.com>
2024-02-12 13:11:22 +0700

    Initial Commit

---
+++ WibuApp/WibuAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WibuAppApp.swift
+//  WibuApp
+//
+//  Created by Pande Adhistanaya on 12/02/24.
+//
+
+import SwiftUI
+
+@main
+struct WibuAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
