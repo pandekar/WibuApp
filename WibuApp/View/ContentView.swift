8a757a7907e3bf5551abad2ed3756506533b6fb3
pandekar <pandeadhistanaya@gmail.com>
2024-02-13 09:27:01 +0700

    search function

--- WibuApp/View/ContentView.swift
+++ WibuApp/View/ContentView.swift
@@ -9,6 +9,17 @@ import SwiftUI
 
 struct ContentView: View {
     @StateObject private var waifuVM = WaifuViewModel()
+    @State private var searchText: String = ""
+    
+    var filteredWaifus: Array<Waifu> {
+        if searchText.isEmpty {
+            return waifuVM.waifus
+        } else {
+            return waifuVM.waifus.filter { waifu in
+                waifu.name.contains(searchText)
+            }
+        }
+    }
     
     let columns: Array<GridItem> = [
         GridItem(.adaptive(minimum: 100), spacing: 10)
@@ -16,58 +27,62 @@ struct ContentView: View {
     let transition = Transaction(animation: .easeInOut(duration: 3.0))
     
     var body: some View {
-        ScrollView {
-            LazyVGrid(columns: columns, spacing: 10) {
-                ForEach(waifuVM.waifus) { waifu in
-                    Group {
-                        let imageUrl = URL(string: waifu.image)
-                        VStack(alignment: .leading) {
-                            AsyncImage(url: imageUrl, transaction: transition) { fetchedWaifu in
-                                switch fetchedWaifu {
-                                case .empty:
-                                    waitView()
-                                     
-                                case .success(let image):
-                                    image.resizable().scaledToFill()
-                                    
-                                case .failure(let error):
-                                    VStack {
-                                        Image(systemName: "photo.fill")
-                                        Text(error.localizedDescription)
+        NavigationStack {
+            ScrollView {
+                LazyVGrid(columns: columns, spacing: 10) {
+                    ForEach(filteredWaifus) { waifu in
+                        Group {
+                            let imageUrl = URL(string: waifu.image)
+                            VStack(alignment: .leading) {
+                                AsyncImage(url: imageUrl, transaction: transition) { fetchedWaifu in
+                                    switch fetchedWaifu {
+                                    case .empty:
+                                        waitView()
+                                         
+                                    case .success(let image):
+                                        image.resizable().scaledToFill()
+                                        
+                                    case .failure(let error):
+                                        VStack {
+                                            Image(systemName: "photo.fill")
+                                            Text(error.localizedDescription)
+                                        }
+                                        
+                                    @unknown default:
+                                        fatalError()
                                     }
-                                    
-                                @unknown default:
-                                    fatalError()
                                 }
+                                .frame(width: 100, height: 100)
+                                .clipShape(RoundedRectangle(cornerRadius: 10)) 
+                                
+                                Text(waifu.name)
+                                    .bold()
+                                Spacer()
+                                Text(waifu.anime)
+                                    .lineLimit(1)
                             }
-                            .frame(width: 100, height: 100)
-                            .clipShape(RoundedRectangle(cornerRadius: 10)) 
-                            
-                            Text(waifu.name)
-                                .bold()
-                            Spacer()
-                            Text(waifu.anime)
-                                .lineLimit(1)
                         }
+                        .padding()
+                        .contextMenu(ContextMenu(menuItems: {
+                            Button {
+                                // code here
+                            } label: {
+                                Label("Share", systemImage: "square.and.arrow.up")
+                            }
+                            Button {
+                                // code here
+                            } label: {
+                                Label("Delete", systemImage: "trash")
+                            }
+                        }))
                     }
-                    .padding()
-                    .contextMenu(ContextMenu(menuItems: {
-                        Button {
-                            // code here
-                        } label: {
-                            Label("Share", systemImage: "square.and.arrow.up")
-                        }
-                        Button {
-                            // code here
-                        } label: {
-                            Label("Delete", systemImage: "trash")
-                        }
-                    }))
                 }
             }
-        }
-        .task {
-            await waifuVM.fetchWaifus()
+            .task {
+                await waifuVM.fetchWaifus()
+            }
+            .navigationTitle("Wibu")
+            .searchable(text: $searchText, placement: .navigationBarDrawer(displayMode: .always))
         }
     }
 }
